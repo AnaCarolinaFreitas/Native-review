e0b5e255aaf9208c6f4b42286d2de3cad37bc687
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-08-14 11:18:49 -0300

    Feat: Products and styles

--- app/_layout.js
+++ app/_layout.js
@@ -8,7 +8,12 @@ export default function Layout() {
                 headerTintColor: "#fff",
                 headerTitleStyle: { fontWeight: "bold" },
             }}
-        />
-        );
-}
+        >
 
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="produtos" options={{ title: "Produtos 🛒" }} />
+        <Stack.Screen name="sobre" options={{ title: "Sobre ℹ️" }} />
+        <Stack.Screen name="contato" options={{ title: "Contato 📞" }} />
+        </Stack>
+    );
+}
--- app/produtos.js
+++ app/produtos.js
@@ -7,23 +7,23 @@ export default function Produtos() {
         <View style={styles.container}>
             <Text style={styles.titulo}>Veja nossos produtos!</Text>
             <Link href="/" asChild>
-                <Button title="⬅️ Voltar para Home" />
+                <Button title="⬅️ Voltar para Home"/>
             </Link>
-            <ScrollView style={styles.productsContainer} contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}>
+            <ScrollView contentContainerStyle={[styles.productsContainer, { alignItems: 'center', justifyContent: 'center' }]}>
             <Image 
                 source={{ uri: 'https://assets.aboutamazon.com/dims4/default/478ffa8/2147483647/strip/false/crop/1280x720+0+0/resize/1200x675!/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2Ff9%2F25%2Fe4aa62e5408ca8764934782e7010%2Fcombo-1280x720.jpg' }}
                 style={{ width: 200, height: 200, marginVertical: 20 }}
             />
             <Image 
-                source={{ uri: 'https://assets.aboutamazon.com/dims4/default/478ffa8/2147483647/strip/false/crop/1280x720+0+0/resize/1200x675!/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2Ff9%2F25%2Fe4aa62e5408ca8764934782e7010%2Fcombo-1280x720.jpg' }}
+                source={{ uri: 'https://ignitemag.co.uk/wp-content/uploads/2024/01/most-wanted-tech-devices2-jpg.webp' }}
                 style={{ width: 200, height: 200, marginVertical: 20 }}
             />
             <Image 
-                source={{ uri: 'https://assets.aboutamazon.com/dims4/default/478ffa8/2147483647/strip/false/crop/1280x720+0+0/resize/1200x675!/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2Ff9%2F25%2Fe4aa62e5408ca8764934782e7010%2Fcombo-1280x720.jpg' }}
+                source={{ uri: 'https://img.freepik.com/premium-photo/toys-kids-play-time-colorful-fun-composition_594847-3791.jpg?w=360' }}
                 style={{ width: 200, height: 200, marginVertical: 20 }}
             />
             <Image 
-                source={{ uri: 'https://assets.aboutamazon.com/dims4/default/478ffa8/2147483647/strip/false/crop/1280x720+0+0/resize/1200x675!/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2Ff9%2F25%2Fe4aa62e5408ca8764934782e7010%2Fcombo-1280x720.jpg' }}
+                source={{ uri: 'https://prh.imgix.net/articles/top10-fiction-1600x800.jpg' }}
                 style={{ width: 200, height: 200, marginVertical: 20 }}
             />
 
@@ -42,6 +42,7 @@ const styles = StyleSheet.create({
     titulo: {
         fontSize: 24,
         fontWeight: 'bold',
+        marginTop: 20,
     },
     subtitulo: {
         fontSize: 18,
